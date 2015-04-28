64f561f4483786d976981d9ff01fe8827e267c84
Ritesh <ritesh.kumar.8467@gmail.com>
2015-04-29 01:49:51 +0530

    oauth added

--- assets/src/js/components/login/login_form.js
+++ assets/src/js/components/login/login_form.js
@@ -1,46 +1,53 @@
+'use strict'
 var React = require('react');
 var InputField = require('../common/input_field');
 var InputButton = require('../common/button');
 var LoginActions = require('../../actions/login-action');
 
 
-module.exports =  React.createClass({
-	_onClick: function(event){
-		event.preventDefault();
-		username = event.target.elements.username.value;
-		password = event.target.elements.password.value;
-		data = {username: username, password: password};
-		LoginActions.signin(data);
-	},
-	render:function(){
-		parentState = this.props.parentState
-		parentClass = "container "
-		if(parentState.loginAwaited){
-			parentClass += "loader"
-		}else if(parentState.totalLoginReqMade && !parentState.loginStatus){
-			parentClass += "error"
-		}
-		return (
-			<div className={parentClass}>
-				<div className="loading"></div>
-				<div className="page-header">
-				    <h3>Sign in</h3>
-				</div>
-				<div className="col-sm-8 col-sm-offset-2">
-		        	<form className={this.props.className} id={this.props.id} onSubmit={this._onClick}>
-		     			<div className={"form-group"}>
-				        <label className={"control-label"}>Username or Email</label>
-				        <InputField id="username" className={"form-control"} placeholder="username" type="text" name="username" autofocus={"autofocus"} />
-				      </div>
-				      <div className="form-group">
-				        <label className={"control-label"}>Password</label>
-				        <InputField id="password" className={"form-control"} placeholder="password" type="password" name="password" autofocus={"autofocus"} />
-				      </div>
-				      <InputButton id="login-btn" className={"btn btn-primary"} value="Submit" value="Sign in" />
-					</form>
-					<div className="error-msg">Username or Password incorrect</div>
-				</div>
-			</div>
-		);
-	}
+module.exports = React.createClass({
+  _onClick: function (event) {
+    event.preventDefault();
+    var username = event.target.elements.username.value,
+      password = event.target.elements.password.value,
+      data = {username: username, password: password};
+    LoginActions.signin(data);
+  },
+  render: function () {
+    var parentState = this.props.parentState,
+      parentClass = "container ";
+    if (parentState.loginAwaited) {
+      parentClass += "loader";
+    } else if (parentState.totalLoginReqMade && !parentState.loginStatus) {
+      parentClass += "error";
+    }
+    return (
+      <div className={parentClass}>
+        <div className="loading"></div>
+        <div className="page-header">
+          <h3>Sign in</h3>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <form className={this.props.className} id={this.props.id} onSubmit={this._onClick}>
+            <div className={"form-group"}>
+              <label className={"control-label"}>Username or Email</label>
+              <InputField id="username" className={"form-control"} placeholder="username" type="text" name="username"
+                          autofocus={"autofocus"}/>
+            </div>
+            <div className="form-group">
+              <label className={"control-label"}>Password</label>
+              <InputField id="password" className={"form-control"} placeholder="password" type="password"
+                          name="password" autofocus={"autofocus"}/>
+            </div>
+            <InputButton id="login-btn" className="btn btn-primary" value="Sign in"/>
+          </form>
+          <div classNam="oauth">
+            <a className="btn btn-primary" href="/auth/google">Sign In with Google</a>
+            <a className="btn btn-primary" href="/auth/facebook">Sign In with Facebook</a>
+          </div>
+          <div className="error-msg">Username or Password incorrect</div>
+        </div>
+      </div>
+    );
+  }
 });
--- server/routes/user_panel_routes.js
+++ server/routes/user_panel_routes.js
@@ -1,53 +1,94 @@
+'use strict'
 var express, router;
-
 express = require('express');
 router = express.Router();
 
-var React         = require('react')
-var path          = require("path");
-var SignUp  = require('../../assets/dist/js/components/signup/signup')
-var Login  = require('../../assets/dist/js/components/login/login')
-var Users  = require('../../assets/dist/js/components/admin/users')
-var Dasboard  = require('../../assets/dist/js/components/dashboard/dashboard')
-var ShowUser  = require('../../assets/dist/js/components/admin/show_user')
+var React, path, SignUp, Login, Users, Dashboard, ShowUser, passport, GoogleStrategy, FacebookStrategy;
+React = require('react');
+path = require("path");
+SignUp = require('../../assets/dist/js/components/signup/signup');
+Login = require('../../assets/dist/js/components/login/login');
+Users = require('../../assets/dist/js/components/admin/users');
+Dashboard = require('../../assets/dist/js/components/dashboard/dashboard');
+ShowUser = require('../../assets/dist/js/components/admin/show_user');
+passport = require('passport');
+GoogleStrategy = require('passport-google').Strategy;
+FacebookStrategy = require('passport-facebook').Strategy;
 
-router.get('/', function(req, res, next) {
-  if(req.authenticated && req.user_data){
-    return (req.user_data.admin ? res.redirect('/users') : res.redirect('/dashboard'))
-  }else{
-    res.redirect('/login');
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    //create user account
+    //User.findOrCreate({ openId: identifier }, function(err, user) {
+    //  done(err, user);
+    //});
   }
+));
+
+router.get('/auth/google', passport.authenticate('google'));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    //create user account
+    //User.findOrCreate(..., function(err, user) {
+    //  if (err) { return done(err); }
+    //  done(null, user);
+    //});
+  }
+));
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+    failureRedirect: '/login' }));
+
+router.get('/auth/google/return',
+  passport.authenticate('google', { successRedirect: '/users',
+    failureRedirect: '/login' }));
+
+router.get('/', function (req, res, next) {
+  if (req.authenticated && req.user_data) {
+    return (req.user_data.admin ? res.redirect('/users') : res.redirect('/dashboard'));
+  }
+  return res.redirect('/login');
 });
 
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
   console.log('******** in user panel ********');
-  var factory = React.createFactory(SignUp);
-  var markup = React.renderToString(factory());
+  var factory = React.createFactory(SignUp),
+    markup = React.renderToString(factory());
   return res.render('index', {
     title: 'SignUp',
     markup: markup,
-    authenticated:  req.authenticated
+    authenticated: req.authenticated
   });
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   console.log('******** in user panel ********');
-  if(req.authenticated){
+  if (req.authenticated) {
     return res.redirect('/users');
   }
-  var factory = React.createFactory(Login);
-  var markup = React.renderToString(factory());
+  var factory = React.createFactory(Login),
+    markup = React.renderToString(factory());
   return res.render('index', {
     title: 'Login',
     markup: markup,
-    authenticated:  req.authenticated
+    authenticated: req.authenticated
   });
 });
 
-router.get('/users', function(req, res, next) {
-  if(!(req.authenticated && req.user_data)){
+router.get('/users', function (req, res, next) {
+  if (!(req.authenticated && req.user_data)) {
     return res.redirect('/login');
-  }else if(req.authenticated && req.user_data && !req.user_data.admin){
+  } else if (req.authenticated && req.user_data && !req.user_data.admin) {
     return res.redirect('/dashboard');
   }
   // var factory = React.createFactory(Users);
@@ -55,26 +96,26 @@ router.get('/users', function(req, res, next) {
   return res.render('index', {
     title: 'Users',
     markup: markup,
-    authenticated:  req.authenticated
+    authenticated: req.authenticated
   });
 });
 
-router.get('/dashboard', function(req, res, next) {
-  if(!(req.authenticated && req.user_data)){
+router.get('/dashboard', function (req, res, next) {
+  if (!(req.authenticated && req.user_data)) {
     return res.redirect('/login');
   }
-  var factory = React.createFactory(Dasboard);
+  var factory = React.createFactory(Dashboard);
   var markup = React.renderToString(factory());
   return res.render('index', {
     title: 'Dashboard',
     markup: markup,
-    authenticated:  req.authenticated,
+    authenticated: req.authenticated,
     admin: false
   });
 });
 
-router.get('/users/:userId', function(req, res, next) {
-  if(!req.authenticated){
+router.get('/users/:userId', function (req, res, next) {
+  if (!req.authenticated) {
     return res.redirect('/login');
   }
   // var factory = React.createFactory(ShowUser);
@@ -82,11 +123,10 @@ router.get('/users/:userId', function(req, res, next) {
   return res.render('index', {
     title: 'User',
     markup: markup,
-    authenticated:  req.authenticated
+    authenticated: req.authenticated
   });
 });
 
-// router.post('/api/v1/authenticate', require('../scripts/user_panel_authentication').init);
 router.post('/api/v1/signin', require('../scripts/authentication').init);
 
 router.get('/api/v1/user-data', require('../scripts/authentication').user_data);
